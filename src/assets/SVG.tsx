87f650b79c02f69ed3a9d8ce0add693c9ee0718b
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-12 10:27:33 +0100

    Add new SVG icons for search, sort, and category

--- src/assets/SVG.tsx
+++ src/assets/SVG.tsx
@@ -242,3 +242,103 @@ export const NotificationsIcon = () => {
     </svg>
   );
 };
+
+export const SearchIcon = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_20_1619)">
+        <path
+          d="M9.58332 17.5001C13.9556 17.5001 17.5 13.9557 17.5 9.58341C17.5 5.21116 13.9556 1.66675 9.58332 1.66675C5.21107 1.66675 1.66666 5.21116 1.66666 9.58341C1.66666 13.9557 5.21107 17.5001 9.58332 17.5001Z"
+          stroke="#8E92BC"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M18.3333 18.3334L16.6667 16.6667"
+          stroke="#8E92BC"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_20_1619">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const SortIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 7H21"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M6 12H18"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M10 17H14"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export const CategoryIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.5 19.9V4.1C10.5 2.6 9.86 2 8.27 2H4.23C2.64 2 2 2.6 2 4.1V19.9C2 21.4 2.64 22 4.23 22H8.27C9.86 22 10.5 21.4 10.5 19.9Z"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22 10.9V4.1C22 2.6 21.36 2 19.77 2H15.73C14.14 2 13.5 2.6 13.5 4.1V10.9C13.5 12.4 14.14 13 15.73 13H19.77C21.36 13 22 12.4 22 10.9Z"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22 19.9V18.1C22 16.6 21.36 16 19.77 16H15.73C14.14 16 13.5 16.6 13.5 18.1V19.9C13.5 21.4 14.14 22 15.73 22H19.77C21.36 22 22 21.4 22 19.9Z"
+        stroke="#8E92BC"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
