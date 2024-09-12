3996a0725a169890bec1ce0236d32dd7ce42c1cc
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-12 10:37:22 +0100

    Refactor Message page to include Header component and dynamic rendering based on route path

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../components/Header";
 
 const Message = () => {
+  const path = useLocation().pathname;
   return (
-    <div className='bg-white flex-[4] p-5'>Message</div>
-  )
-}
+    <div className="bg-white flex-[4] p-5 flex flex-col">
+      <Header title={"Message"} path={path} />
+      Message
+    </div>
+  );
+};
 
-export default Message
+export default Message;
