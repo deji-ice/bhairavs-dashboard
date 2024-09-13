574398dc1a96e8499a8f0f83737f370fe1d2eeb0
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-13 13:03:16 +0100

    ```text Refactor Task component and add Header

--- src/pages/tasks/Task.tsx
+++ src/pages/tasks/Task.tsx
@@ -1,10 +1,14 @@
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import TaskData from "./TaskData";
+import Header from "../../components/Header";
+import { useLocation } from "react-router-dom";
 
 const Task = () => {
+  const path = useLocation().pathname;
   return (
     <>
-    <div className="bg-gray-300/10 flex-[4] p-5">
+    <Header path={path} title="hfjd"/>
+    <div className="bg-gray-300/10 flex-[4] mt-[8rem] p-5">
       <div className="p-5 flex flex-col gap-7">
         <div className="flex justify-between">
           <h2>Time Limit</h2>
