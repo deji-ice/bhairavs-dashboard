8a77950800a28dfcd71cc2f9950633cfb638ee2c
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-14 01:17:09 +0100

    Add PopUpModal component and integrate it into App; implement auto-close functionality

--- src/pages/tasks/Task.tsx
+++ src/pages/tasks/Task.tsx
@@ -7,23 +7,23 @@ const Task = () => {
   const path = useLocation().pathname;
   return (
     <>
-    <Header path={path} title="hfjd"/>
-    <div className="bg-gray-300/10 flex-[4] mt-[8rem] p-5">
-      <div className="p-5 flex flex-col gap-7">
-        <div className="flex justify-between">
-          <h2>Time Limit</h2>
-          <span className="flex gap-3">
-            <button>
-              <FaArrowLeft />
-            </button>
-            <button>
-              <FaArrowRight />
-            </button>
-          </span>
+      <Header path={path} title="hfjd" />
+      <div className="bg-gray-300/10 flex-[4] mt-[8rem] p-5">
+        <div className="p-5 flex flex-col gap-7">
+          <div className="flex justify-between">
+            <h2>Time Limit</h2>
+            <span className="flex gap-3">
+              <button>
+                <FaArrowLeft />
+              </button>
+              <button>
+                <FaArrowRight />
+              </button>
+            </span>
+          </div>
+          <TaskData />
         </div>
-        <TaskData/>
       </div>
-    </div>
     </>
   );
 };
